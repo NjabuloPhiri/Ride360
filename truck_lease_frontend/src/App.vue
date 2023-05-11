085bc21931047537d1993a6c261fcadeef9b8f11
NjabuloPhiri <43642074+NjabuloPhiri@users.noreply.github.com>
2023-05-11 23:15:07 +0200

    Initial commit [Sponsors Frontend]

---
+++ truck_lease_frontend/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <!---Create navbar containing menu icons-->
+  <nav class="py-10 px-8 border-b border-gray-200">
+    <div class="max-w-7xl mx-auto">
+      <div class="flex items-center justify-between">
+        <div class="menu-left">
+          <a href="#" class="text-xl">Ride360</a>
+        </div>
+
+        <div class="menu-center flex space-x-12">
+          <a href="#" class="text-green-700">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+            </svg>
+          </a>
+
+          <a href="#" class="text-green-700">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+            </svg>
+          </a>
+
+          <a href="#" class="text-green-700">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
+              <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
+            </svg>
+          </a>          
+        </div>
+
+        <div class="menu-right">
+          <a href="#">
+            <img src="https://i.pravatar.cc/40?img=64" class="rounded-full" alt="user-avatar">
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </nav>
+
+  <main class="px-8 py-6 bg-gray-100">
+    <RouterView/>
+  </main>
+</template>
+
+
---
+++ truck_lease_frontend/src/views/SponsorsView.vue
@@ -0,0 +1,36 @@
+<template>
+    <main>
+        <div class="max-w-7xl mx-auto ">
+            <div class="upper-div flex gap-4 ">
+                
+                <div class="left-menu-panel p-12 p-12 bg-green-900 max-h-full w-40 border border-gray-100 rounded-lg">
+                    <!--p-12 bg-green-900 h-96 w-40 border border-gray-100 rounded-lg-->
+                    Menu
+                </div>
+                <div class="wrapper-test w-screen mx-auto space-y-4">
+                    <div class="top-panel p-12 bg-green-900 h-24 w-full border border-gray-100 rounded-lg">
+                        Top Panel
+                    </div>
+
+                    <div class="investment-details-panel p-12 bg-green-900 h-32 w-full border border-gray-100 rounded-lg">
+                        Investment Panel
+                    </div>
+
+                    <div class="visualization-panels mx-auto grid grid-cols-2 space-x-4">
+                        <div class="graph-visualizer p-12 bg-green-900 h-40 max-w-2xl border border-gray-100 rounded-lg">
+                            Graph visualizer
+                        </div>
+                        <div class="chart-visualizer p-12 bg-green-900 h-40 max-w-2xl border border-gray-100 rounded-lg">
+                            Chart visualizer
+                        </div>
+                    </div>
+                </div>
+
+                
+                
+                
+                
+            </div>
+        </div>
+    </main>
+</template>
